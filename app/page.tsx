b8a1ceaad04ba34d78a2a34f7de763bd046748a9
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-02-06 22:21:34 +0530

    Added a main container along with its parts.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,14 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>hello</h1>
+      <div className="w-full h-screen flex gap-96">
+        <div className="left h-screen">
+          <h2>hi</h2>
+        </div>
+        <div className="right h-screen ">
+          <h1>hello</h1>
+        </div>
+      </div>
     </main>
   );
 }
