837daf8c30774dd3195ccc791d657ef092e9e927
ANINDITAPRIYADARSHINI <anindita.priyadarshini8@gmail.com>
2025-02-07 18:44:33 +0530

    Added the card elements to the right side.

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,20 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-full h-screen flex gap-96">
         <div className="left h-screen">
-          <h2>hi</h2>
+          <h1>Hello World!</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quasi id, ea unde deserunt possimus voluptatum quae eligendi explicabo. Animi repellat rerum cum corrupti debitis incidunt reiciendis, veniam molestias voluptates.</p>
+          <button>Learn More</button>
         </div>
         <div className="right h-screen ">
-          <h1>hello</h1>
+          <h3>Hi, Abc</h3>
+          <h1>How is it going?</h1>
+          <div className="w-80 h-80 rounded-3xl bg-yellow-200 absolute"></div>
+          <div className="w-80 h-80 rounded-3xl bg-green-200 absolute"></div>
+          <div className="w-80 h-80 rounded-3xl bg-blue-300 absolute"></div>
+          <div className="w-80 h-80 rounded-3xl bg-purple-400 absolute"></div>
+          <div className="w-80 h-80 rounded-3xl bg-pink-400 absolute"></div>
+          <div className="w-80 h-80 rounded-3xl bg-indigo-400 absolute"></div>
+
         </div>
       </div>
     </main>
